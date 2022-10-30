91eb28eef54a8b7e751422e1b0f69c8d678ebe37
kylejrhoden <114625580+kylejrhoden@users.noreply.github.com>
2022-10-30 11:07:51 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const emailVal = email.value;
+
+    //check if it is a valid email
+
+    if(!validateEmail(emailVal)) {
+        form.classList.add('error');
+    } else {
+        form.classList.remove('error');
+    }
+
+})
+
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
